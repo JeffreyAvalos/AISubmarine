538b9c61d15a4ada722deafe1791585a2b03b2ee
JeffreyAvalos <33049132+JeffreyAvalos@users.noreply.github.com>
2019-02-13 13:53:35 -0600

    Add files via upload
    Initial push

---
+++ MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+   private void Start()
+   {
+	   //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	   
+	   //Scene newScene = SceneManager.CreateScene("SceneManager");
+	   //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+   }
+   
+   public void StartGame()
+   {
+	   SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+   }
+   
+   public void Exit()
+   {
+	   Debug.Log("Exit!");
+	   Application.Quit();
+   }
+}
---
+++ PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed = 3.0f;
+	public float obstacleRange = 3.0f;
+	void Update() 
+	{
+		transform.Translate(0, 0, speed * Time.deltaTime);
+		
+		Ray ray = new Ray(transform.position, transform.forward);
+		RaycastHit hit;
+		
+		if (Physics.SphereCast(ray, 0.5f, out hit)) 
+		{
+			if (hit.distance < obstacleRange) 
+			{
+				float angle = Random.Range(-90, 90);
+				transform.Rotate(0, angle, 0);
+			}
+		}
+	}
+}
+
+/*public class PlayerController : MonoBehaviour
+{
+	public float speed;
+	private Rigidbody rb;
+	
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	
+	void Update() 
+	{
+		Vector3 direction = new Vector3 (0.0f, 0.0f, 1.0f);
+		rb.AddForce(direction * speed);
+	} 
+}	
+*/	
+	
+	/*void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+			
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+			
+		rb.AddForce(movement*speed);
+	}*/
+
